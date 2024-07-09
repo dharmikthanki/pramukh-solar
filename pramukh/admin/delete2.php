ac78461497e2bcb1c2ac3abb0978fa3dcc91d338
Dharmik Thanki <88383036+dharmikthanki@users.noreply.github.com>
2024-07-09 09:08:12 +0530

    Delete pramukh directory

--- pramukh/admin/delete2.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('db.php');
-$id=$_REQUEST['id'];
-$query = "DELETE FROM book_form WHERE id=$id"; 
-$result = mysqli_query($con,$query) or die ( mysqli_error($con));
-header("Location: app.php"); 
-?> 
--- pramukh/admin/delete_contact.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('db.php');
-$id=$_REQUEST['id'];
-$query = "DELETE FROM pramukhtbl WHERE id=$id"; 
-$result = mysqli_query($con,$query) or die ( mysqli_error($con));
-header("Location: show_contacts.php"); 
-?> 
